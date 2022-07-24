13efc6ded69be0da45c62d143aaf9eb64bad4947
peedromarinhow <peedromarinhow@gmail.com>
2022-07-24 19:41:32 -0300

    Documentations.

--- main.c
+++ main.c
@@ -2,21 +2,17 @@
 
 int main(int Argc, char **Argv) {
   SysInit(Argc, Argv);
-
   window *Window = SysCreateWindowWithOpenGL();
-
-  r32 Red = 0;
-
+  r32 RedCycle = 0;
   while (!Window->Quit) {
     SysGetInput(Window);
     SysBeginRenderingWithOpengl(Window);
-    Window->GlClearColor(Red, 0, 0, 1);
+    Window->GlClearColor(RedCycle, 0, 0, 1);
     Window->GlClear(GL_COLOR_BUFFER_BIT);
     SysEndRenderingWithOpengl(Window);
-    Red += 0.05f;
-    if (Red >= 0.9f)
-      Red = 0;
+    RedCycle += 0.05f;
+    if (RedCycle >= 0.9f)
+      RedCycle = 0;
   }
-
   SysEnd();
 }
